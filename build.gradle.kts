f4a8a92a0e0a72cfc12a1738ef1192ca691bb320
ZhangJian He <shoothzj@gmail.com>
2023-05-16 13:40:15 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    checkstyle
+}
+
+group = "io.github.protocol-laboratory"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
